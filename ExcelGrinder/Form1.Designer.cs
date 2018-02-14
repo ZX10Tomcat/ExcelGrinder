197cd4bfe4a67bcc8ca61be3a464a045c3bf6757
Bogdan Samoletskyi <bogdan.samoletsky@actum.cz>
2018-02-14 17:26:20 +0100

    First commit

---
+++ ExcelGrinder/Form1.Designer.cs
@@ -0,0 +1,179 @@
+﻿namespace ExcelGrinder
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.ChoseFilebtn = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.ExcelRuleBookView = new System.Windows.Forms.DataGridView();
+            this.InfoLabel = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.ExcelOutputView = new System.Windows.Forms.DataGridView();
+            this.Grindbtn = new System.Windows.Forms.Button();
+            this.ChoseFolderbtn = new System.Windows.Forms.Button();
+            this.Cancelbtn = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ExcelRuleBookView)).BeginInit();
+            this.groupBox2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.ExcelOutputView)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // ChoseFilebtn
+            // 
+            this.ChoseFilebtn.Location = new System.Drawing.Point(9, 29);
+            this.ChoseFilebtn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.ChoseFilebtn.Name = "ChoseFilebtn";
+            this.ChoseFilebtn.Size = new System.Drawing.Size(273, 71);
+            this.ChoseFilebtn.TabIndex = 0;
+            this.ChoseFilebtn.Text = "Открыть файл с фамилиями";
+            this.ChoseFilebtn.UseVisualStyleBackColor = true;
+            this.ChoseFilebtn.Click += new System.EventHandler(this.ChoseFilebtn_Click);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.ExcelRuleBookView);
+            this.groupBox1.Controls.Add(this.ChoseFilebtn);
+            this.groupBox1.Location = new System.Drawing.Point(20, 20);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox1.Size = new System.Drawing.Size(486, 686);
+            this.groupBox1.TabIndex = 1;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Список фамилий";
+            // 
+            // ExcelRuleBookView
+            // 
+            this.ExcelRuleBookView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ExcelRuleBookView.Location = new System.Drawing.Point(10, 111);
+            this.ExcelRuleBookView.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.ExcelRuleBookView.Name = "ExcelRuleBookView";
+            this.ExcelRuleBookView.Size = new System.Drawing.Size(466, 562);
+            this.ExcelRuleBookView.TabIndex = 1;
+            // 
+            // InfoLabel
+            // 
+            this.InfoLabel.AutoSize = true;
+            this.InfoLabel.Location = new System.Drawing.Point(20, 722);
+            this.InfoLabel.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.InfoLabel.Name = "InfoLabel";
+            this.InfoLabel.Size = new System.Drawing.Size(0, 20);
+            this.InfoLabel.TabIndex = 2;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.ExcelOutputView);
+            this.groupBox2.Controls.Add(this.Grindbtn);
+            this.groupBox2.Controls.Add(this.ChoseFolderbtn);
+            this.groupBox2.Location = new System.Drawing.Point(516, 20);
+            this.groupBox2.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Padding = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.groupBox2.Size = new System.Drawing.Size(788, 686);
+            this.groupBox2.TabIndex = 3;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Вывод";
+            // 
+            // ExcelOutputView
+            // 
+            this.ExcelOutputView.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.ExcelOutputView.Location = new System.Drawing.Point(9, 108);
+            this.ExcelOutputView.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.ExcelOutputView.Name = "ExcelOutputView";
+            this.ExcelOutputView.Size = new System.Drawing.Size(770, 562);
+            this.ExcelOutputView.TabIndex = 2;
+            // 
+            // Grindbtn
+            // 
+            this.Grindbtn.BackColor = System.Drawing.Color.Lavender;
+            this.Grindbtn.Location = new System.Drawing.Point(321, 29);
+            this.Grindbtn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Grindbtn.Name = "Grindbtn";
+            this.Grindbtn.Size = new System.Drawing.Size(458, 69);
+            this.Grindbtn.TabIndex = 1;
+            this.Grindbtn.Text = "НАЧАТЬ!";
+            this.Grindbtn.UseVisualStyleBackColor = false;
+            this.Grindbtn.Click += new System.EventHandler(this.Grindbtn_Click);
+            // 
+            // ChoseFolderbtn
+            // 
+            this.ChoseFolderbtn.Location = new System.Drawing.Point(9, 31);
+            this.ChoseFolderbtn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.ChoseFolderbtn.Name = "ChoseFolderbtn";
+            this.ChoseFolderbtn.Size = new System.Drawing.Size(272, 69);
+            this.ChoseFolderbtn.TabIndex = 0;
+            this.ChoseFolderbtn.Text = "Выбрать папку с файлами";
+            this.ChoseFolderbtn.UseVisualStyleBackColor = true;
+            this.ChoseFolderbtn.Click += new System.EventHandler(this.ChoseFolderbtn_Click);
+            // 
+            // Cancelbtn
+            // 
+            this.Cancelbtn.BackColor = System.Drawing.Color.Red;
+            this.Cancelbtn.Location = new System.Drawing.Point(1150, 715);
+            this.Cancelbtn.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Cancelbtn.Name = "Cancelbtn";
+            this.Cancelbtn.Size = new System.Drawing.Size(153, 40);
+            this.Cancelbtn.TabIndex = 4;
+            this.Cancelbtn.Text = "ОТМЕНА";
+            this.Cancelbtn.UseVisualStyleBackColor = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1322, 760);
+            this.Controls.Add(this.Cancelbtn);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.InfoLabel);
+            this.Controls.Add(this.groupBox1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.Name = "Form1";
+            this.Text = "Excel grinder";
+            this.groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.ExcelRuleBookView)).EndInit();
+            this.groupBox2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.ExcelOutputView)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button ChoseFilebtn;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.DataGridView ExcelRuleBookView;
+        private System.Windows.Forms.Label InfoLabel;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button Grindbtn;
+        private System.Windows.Forms.Button ChoseFolderbtn;
+        private System.Windows.Forms.Button Cancelbtn;
+        private System.Windows.Forms.DataGridView ExcelOutputView;
+    }
+}
+
---
+++ ExcelGrinder/Form1.cs
@@ -0,0 +1,225 @@
+﻿using NPOI.XSSF.UserModel;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ExcelGrinder
+{
+    public partial class Form1 : Form
+    {
+
+        string selectedPath;
+        string[] files;
+        DataTable DT = new DataTable();
+        string surnameFile = string.Empty;
+
+        string newFileName = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\\ExcelOutFile_" + DateTime.Now.ToString("yyyy_MM_dd") + ".xlsx";
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        #region SurnameList
+        /* 1. Chose file with list
+         * 2. read file and show it in the grid
+         */
+        private void ChoseFilebtn_Click(object sender, EventArgs e)
+        {
+            Stream myStream = null;
+            surnameFile = string.Empty;
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            openFileDialog1.InitialDirectory = "c:\\";
+            openFileDialog1.Filter = "txt files (*.txt)|*.txt|All files (*.*)|*.*";
+            openFileDialog1.FilterIndex = 2;
+            openFileDialog1.RestoreDirectory = true;
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    if ((myStream = openFileDialog1.OpenFile()) != null)
+                    {
+                        using (myStream)
+                        {
+                            XSSFWorkbook wb = new XSSFWorkbook(myStream);
+                            InfoLabel.Text = "Файл: " + openFileDialog1.FileName;
+                            surnameFile = openFileDialog1.FileName;
+                            GetDataFromRuleBook(wb);
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error: Could not read file from disk. Original error: " + ex.Message);
+                }
+            }
+        }
+
+        private void GetDataFromRuleBook(XSSFWorkbook wb)
+        {
+            var sheet = wb.GetSheet(wb.GetSheetName(0));
+            var source = new BindingSource();
+
+            DT.Rows.Clear();
+            DT.Columns.Clear();
+
+            for (int i = 0; i < sheet.LastRowNum; i++)
+            {
+                // add neccessary columns
+                if (DT.Columns.Count < sheet.GetRow(i).Cells.Count)
+                {
+                    for (int j = 0; j < sheet.GetRow(i).Cells.Count; j++)
+                    {
+                        DT.Columns.Add("", typeof(string));
+                    }
+                }
+
+                // add row
+                DT.Rows.Add();
+
+                // write row value
+                for (int j = 0; j < sheet.GetRow(i).Cells.Count; j++)
+                {
+                    var cell = sheet.GetRow(i).GetCell(j);
+
+                    if (cell != null)
+                    {
+                        // TODO: you can add more cell types capatibility, e. g. formula
+                        switch (cell.CellType)
+                        {
+                            case NPOI.SS.UserModel.CellType.Numeric:
+                                DT.Rows[i][j] = sheet.GetRow(i).GetCell(j).NumericCellValue;
+                                //dataGridView1[j, i].Value = sh.GetRow(i).GetCell(j).NumericCellValue;
+
+                                break;
+                            case NPOI.SS.UserModel.CellType.String:
+                                DT.Rows[i][j] = sheet.GetRow(i).GetCell(j).StringCellValue;
+
+                                break;
+                        }
+                    }
+                }
+            }
+            /*List<Object> rowList = new List<object>();
+            for (int i = 0; i < sheet.LastRowNum; i++)
+            {
+                var row = sheet.GetRow(i);
+                var cells = row.Cells.ToList();
+                rowList.Add(cells);
+            }
+
+            source.DataSource = rowList;
+            */
+            ExcelRuleBookView.DataSource = DT;
+        }
+        #endregion
+        #region ExcelGrind
+        /* 1. Chose folder
+         * 2. Go through list of names {
+         * 3.    Go through excel files in folder {
+         * 4.         Search name in files
+         * 5.         After name found, find first and last line
+         * 6.         Copy and past line range to a new file 
+         * 7.         Show info in grid
+         *       } 
+         *       if name not found, write it in the file and inform user
+         *    }    
+         */
+
+        private void ChoseFolderbtn_Click(object sender, EventArgs e)
+        {
+            using (var fbd = new FolderBrowserDialog())
+            {
+
+                fbd.SelectedPath = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments);
+                DialogResult result = fbd.ShowDialog();
+
+                if (result == DialogResult.OK && !string.IsNullOrWhiteSpace(fbd.SelectedPath))
+                {
+                    files = Directory.GetFiles(fbd.SelectedPath);
+
+                    InfoLabel.Text = "В папке: " + fbd.SelectedPath + " найдено " + files.Length.ToString() + " файлов.";
+                    selectedPath = fbd.SelectedPath;
+                }
+            }
+        }
+
+        private void Grindbtn_Click(object sender, EventArgs e)
+        {
+            InfoLabel.Text = selectedPath;
+            if (string.IsNullOrEmpty(selectedPath))
+            {
+                MessageBox.Show("Папка с файлами не выбрана");
+                return;
+            }
+            if (files.Length == 0)
+            {
+                MessageBox.Show("Папка с файлами пуста");
+                return;
+            }
+            if (DT.Rows.Count == 0)
+            {
+                MessageBox.Show("Файл с фамилиями не выбран или пуст");
+                return;
+            }
+            
+
+
+            foreach (DataRow row in DT.Rows)
+            {
+                if (row[2].ToString().Contains(value: "Цикловая") || row[2].ToString().Contains(value: "Студклуб"))
+                {
+                    continue;
+                }
+
+                foreach (var fileName in files)
+                {
+                    if (!Path.GetExtension(fileName).Contains("xls") || fileName == surnameFile)
+                    {
+                        continue;
+                    }
+                    
+                    InfoLabel.Text = "Ищу: " + row[2].ToString() + " в файле: " + fileName;
+                    bool nameFound = SearchNameInFile(fileName);
+                    if (nameFound)
+                    {
+                        findRange(fileName);
+                        copyRange();
+                        showInfoInGrid();
+                    }                    
+                }                
+            }
+        }
+
+        private void showInfoInGrid()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void copyRange()
+        {
+            throw new NotImplementedException();
+        }
+
+        private void findRange(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+        private bool SearchNameInFile(string fileName)
+        {
+            throw new NotImplementedException();
+        }
+
+
+        #endregion
+    }
+}
